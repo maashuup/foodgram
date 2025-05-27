f81bab67b8dffed4381837e0ff3d8d916f02d32f
Mariia Znobishcheva <lanvinmasha@gmail.com>
2025-05-27 16:16:39 +0300

    Correction of static

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -95,7 +95,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/app/static/backend'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
