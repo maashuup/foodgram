f3096ab1e4b63bc0eb9a102969e46f4d03a17ef4
Mariia Znobishcheva <lanvinmasha@gmail.com>
2025-06-16 19:31:14 +0300

    Удалила outer.register('users', UserViewSet)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import IngredientViewSet, RecipeViewSet, TagViewSet, UserViewSet
+from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 router = DefaultRouter()
-router.register('users', UserViewSet)
+# router.register('users', UserViewSet)
 router.register('recipes', RecipeViewSet)
 router.register('tags', TagViewSet)
 router.register('ingredients', IngredientViewSet)
