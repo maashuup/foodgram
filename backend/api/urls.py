23dd4a704148050c3570c96f1162064a321a9f54
Mariia Znobishcheva <lanvinmasha@gmail.com>
2025-06-16 19:50:46 +0300

    Вернула outer.register('users', UserViewSet) и поменяла местами urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,17 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
+from api.views import IngredientViewSet, RecipeViewSet, TagViewSet, UserViewSet
 
 router = DefaultRouter()
-# router.register('users', UserViewSet)
+router.register('users', UserViewSet)
 router.register('recipes', RecipeViewSet)
 router.register('tags', TagViewSet)
 router.register('ingredients', IngredientViewSet)
 
 
 urlpatterns = [
-    path('api/', include(router.urls)),
     path('api/', include('djoser.urls')),
+    path('api/', include(router.urls)),
     path('api/auth/', include('djoser.urls.authtoken')),
 ]
